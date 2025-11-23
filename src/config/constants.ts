986e6daedf1a19ba0d6c98fa9b1652d04d791707
jeremiah-eth <ihejierechibueze@gmail.com>
2025-11-23 03:56:15 -0500

    feat: add app-wide constants

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = 'ChainMeet'
+export const MAX_PHOTO_SIZE = 5 * 1024 * 1024 // 5MB
+export const SUPPORTED_CHAINS = [1, 42161] // Mainnet, Arbitrum
+
+export const TOKENS = {
+    USDC: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    WETH: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+}
